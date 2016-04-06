944cbd0e94106cd120949834c54ee206f8a48537
Riley Tomasek <rt@dropbox.com>
2016-04-06 15:36:12 -0700

    Allow pluggable rpcRequest function

--- src/dropbox-api.js
+++ src/dropbox-api.js
@@ -2,6 +2,7 @@ var rpcRequest = require('./rpc-request');
 
 var DropboxApi = function (options) {
   this.accessToken = options && options.accessToken || '';
+  this.rpcRequest = options && options.rpcRequest || rpcRequest;
 };
 
 DropboxApi.prototype.setAccessToken = function (accessToken) {
@@ -12,8 +13,12 @@ DropboxApi.prototype.getAccessToken = function () {
   return this.accessToken;
 };
 
+DropboxApi.prototype.setRpcRequest = function (rpcRequest) {
+  this.rpcRequest = rpcRequest;
+};
+
 DropboxApi.prototype.listFolder = function (path) {
-  return rpcRequest('files/list_folder', { path: path }, this.getAccessToken());
+  return this.rpcRequest('files/list_folder', { path: path }, this.getAccessToken());
 };
 
 module.exports = DropboxApi;
