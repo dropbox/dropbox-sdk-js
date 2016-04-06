7bd2ebbd4c4a480d74866a2f3a7810eb1584e5d4
Riley Tomasek <rt@dropbox.com>
2016-04-06 16:04:21 -0700

    Create jquery backed rcpRequest and example

--- src/dropbox-api.js
+++ src/dropbox-api.js
@@ -1,4 +1,5 @@
 var rpcRequest = require('./rpc-request');
+var jqueryRpcRequest = require('./rpc-request-jquery');
 
 var DropboxApi = function (options) {
   this.accessToken = options && options.accessToken || '';
@@ -15,6 +16,13 @@ DropboxApi.prototype.getAccessToken = function () {
 
 DropboxApi.prototype.setRpcRequest = function (rpcRequest) {
   this.rpcRequest = rpcRequest;
+};
+
+// Realistically this will be a lib that the client produces and adds, but im
+// putting it in here like this to avoid having to build it on my own for this
+// example.
+DropboxApi.prototype.useJqueryRpc = function () {
+  this.setRpcRequest(jqueryRpcRequest);
 };
 
 DropboxApi.prototype.listFolder = function (path) {
