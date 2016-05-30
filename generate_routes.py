85b21f1f359b5954ab6707465f8d4eda3b72b929
Ken Elkabany <ken@elkabany.com>
2016-05-29 21:29:54 -0700

    Added generate_routes.py script to run Stone.
    - Generates routes.js without team auth routes nor upload/download style routes.

---
+++ generate_routes.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import argparse
+import glob
+import os
+import subprocess
+import sys
+
+cmdline_desc = """\
+Runs Stone to generate JS routes for the Dropbox client. 
+"""
+
+_cmdline_parser = argparse.ArgumentParser(description=cmdline_desc)
+_cmdline_parser.add_argument(
+    '-v',
+    '--verbose',
+    action='store_true',
+    help='Print debugging statements.',
+)
+_cmdline_parser.add_argument(
+    'spec',
+    nargs='*',
+    type=str,
+    help='Path to API specifications. Each must have a .stone extension.',
+)
+_cmdline_parser.add_argument(
+    '-s',
+    '--stone',
+    type=str,
+    help='Path to clone of stone repository.',
+)
+
+
+def main():
+    """The entry point for the program."""
+
+    args = _cmdline_parser.parse_args()
+    verbose = args.verbose
+
+    if args.spec:
+        specs = args.spec
+    else:
+        # If no specs were specified, default to the spec submodule.
+        specs = glob.glob('spec/*.stone')  # Arbitrary sorting
+        specs.sort()
+
+    specs = [os.path.join(os.getcwd(), s) for s in specs]
+
+    stone_path = os.path.abspath('stone')
+    if args.stone:
+        stone_path = args.stone
+
+    dropbox_pkg_path = os.path.abspath(
+        os.path.join(os.path.dirname(sys.argv[0]), 'src'))
+    if verbose:
+        print('Dropbox package path: %s' % dropbox_pkg_path)
+
+    if verbose:
+        print('Generating JS client routes')
+    subprocess.check_output(
+        (['python', '-m', 'stone.cli', 'js_client', dropbox_pkg_path] +
+         specs + ['-b', 'team', '-f', 'style!="upload" and style!="download"'] +
+         ['--', 'routes.js', '-a', 'host', '-a', 'style']),
+        cwd=stone_path)
+
+if __name__ == '__main__':
+    main()
--- generator/run_generator.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-babelapi js.babelg.py ../src ../dropbox-api-spec/*.babel -f 'style!="download" and style!="upload"' -- routes.js -a style
