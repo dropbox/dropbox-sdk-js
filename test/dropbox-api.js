acc72b53b6c031bb1ffe5af75df529bb891aaa32
Riley Tomasek <riley.tomasek@gmail.com>
2016-06-01 17:12:51 -0400

    Fix lint problems in tests

--- test/dropbox-api.js
+++ test/dropbox-api.js
@@ -1,24 +1,26 @@
-var DropboxApi = require('../src/dropbox-api');
+/* eslint-env mocha */
+var Dropbox = require('../src/index');
 var REQUEST_CONSTANTS = require('../src/request-constants');
 var chai = require('chai');
 var sinon = require('sinon');
 
 var assert = chai.assert;
 
-describe('DropboxApi', function () {
+describe('Dropbox', function () {
+  var dbx;
   describe('.accessToken', function () {
     it('defaults to an empty string', function () {
-      dbx = new DropboxApi();
+      dbx = new Dropbox();
       assert.equal(dbx.getAccessToken(), '');
     });
 
     it('can be set in the constructor', function () {
-      dbx = new DropboxApi({ accessToken: 'foo' });
+      dbx = new Dropbox({ accessToken: 'foo' });
       assert.equal(dbx.getAccessToken(), 'foo');
     });
 
     it('can be set after being instantiated', function () {
-      dbx = new DropboxApi();
+      dbx = new Dropbox();
       dbx.setAccessToken('foo');
       assert.equal(dbx.getAccessToken(), 'foo');
     });
@@ -26,13 +28,13 @@ describe('DropboxApi', function () {
 
   describe('.rpcRequest', function () {
     it('defaults to the libraries implementation', function () {
-      dbx = new DropboxApi();
+      dbx = new Dropbox();
       assert.equal(dbx.getAccessToken(), '');
     });
 
     it('can be set to something else by the user', function () {
       var aFunction = function () {};
-      dbx = new DropboxApi();
+      dbx = new Dropbox();
       dbx.setRpcRequest(aFunction);
       assert.equal(dbx.getRpcRequest(), aFunction);
     });
@@ -40,25 +42,25 @@ describe('DropboxApi', function () {
 
   describe('.getAuthenticationUrl()', function () {
     it('throws an error if the client id isn\'t set', function () {
-      dbx = new DropboxApi();
+      dbx = new Dropbox();
       assert.throws(
-        DropboxApi.prototype.getAuthenticationUrl.bind(dbx, 'https://redirecturl.com'),
+        Dropbox.prototype.getAuthenticationUrl.bind(dbx, 'https://redirecturl.com'),
         Error,
         'A client id is required. You can set the client id using .setClientId().'
       );
     });
 
     it('throws an error if the redirect url isn\'t set', function () {
-      dbx = new DropboxApi({ clientId: 'CLIENT_ID' });
+      dbx = new Dropbox({ clientId: 'CLIENT_ID' });
       assert.throws(
-        DropboxApi.prototype.getAuthenticationUrl.bind(dbx),
+        Dropbox.prototype.getAuthenticationUrl.bind(dbx),
         Error,
         'A redirect uri is required.'
       );
     });
 
     it('returns auth url with redirect uri', function () {
-      dbx = new DropboxApi({ clientId: 'CLIENT_ID' });
+      dbx = new Dropbox({ clientId: 'CLIENT_ID' });
       assert.equal(
         dbx.getAuthenticationUrl('redirect'),
         'https://www.dropbox.com/oauth2/authorize?response_type=token&client_id=CLIENT_ID&redirect_uri=redirect'
@@ -66,7 +68,7 @@ describe('DropboxApi', function () {
     });
 
     it('returns auth url with redirect uri and state', function () {
-      dbx = new DropboxApi({ clientId: 'CLIENT_ID' });
+      dbx = new Dropbox({ clientId: 'CLIENT_ID' });
       assert.equal(
         dbx.getAuthenticationUrl('redirect', 'state'),
         'https://www.dropbox.com/oauth2/authorize?response_type=token&client_id=CLIENT_ID&redirect_uri=redirect&state=state'
@@ -76,17 +78,18 @@ describe('DropboxApi', function () {
 
   describe('.request()', function () {
     it('calls the correct request method', function () {
-      dbx = new DropboxApi();
-      var rpcSpy = sinon.spy(dbx, 'rpcRequest');
+      var rpcSpy;
+      dbx = new Dropbox();
+      rpcSpy = sinon.spy(dbx, 'rpcRequest');
       dbx.request('path', {}, REQUEST_CONSTANTS.RPC);
       assert(rpcSpy.calledOnce);
       assert.equal('path', dbx.rpcRequest.getCall(0).args[0]);
       assert.deepEqual({}, dbx.rpcRequest.getCall(0).args[1]);
     });
     it('throws an error for invalid request types', function () {
-      dbx = new DropboxApi();
+      dbx = new Dropbox();
       assert.throws(
-        DropboxApi.prototype.request.bind(DropboxApi, '', {}, 'BADTYPE'),
+        Dropbox.prototype.request.bind(Dropbox, '', {}, 'BADTYPE'),
         Error,
         'Invalid request type'
       );
@@ -94,9 +97,10 @@ describe('DropboxApi', function () {
   });
 
   describe('api method', function () {
-    it('filesListFolder calls DropboxApi.request', function () {
-      dbx = new DropboxApi();
-      var requestSpy = sinon.spy(dbx, 'request');
+    it('filesListFolder calls Dropbox.request', function () {
+      var requestSpy;
+      dbx = new Dropbox();
+      requestSpy = sinon.spy(dbx, 'request');
       dbx.filesListFolder({});
       assert(requestSpy.calledOnce);
       assert.equal('files/list_folder', dbx.request.getCall(0).args[0]);
