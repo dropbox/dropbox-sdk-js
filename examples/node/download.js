0c0c6b7a9810d207444e7b750428d126b8bf1384
Riley Tomasek <riley.tomasek@gmail.com>
2016-07-26 15:21:56 -0400

    Add node download example

---
+++ examples/node/download.js
@@ -0,0 +1,32 @@
+var Dropbox = require('../../src/dropbox');
+var fs = require('fs');
+var prompt = require('prompt');
+
+prompt.start();
+
+prompt.get({
+  properties: {
+    accessToken: {
+      description: 'Please enter an API V2 access token'
+    },
+    sharedLink: {
+      description: 'Please enter a shared link to a png'
+    }
+  }
+}, function (error, result) {
+  var dbx = new Dropbox({ accessToken: result.accessToken });
+  dbx.sharingGetSharedLinkFile({ url: result.sharedLink })
+    .then(function (response) {
+      var file = fs.createWriteStream('foo.png');
+      response.on('data', function (chunk) {
+        file.write(chunk);
+      }).on('close', function () {
+        file.end();
+      });
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+});
+
+
