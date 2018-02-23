a03c2033333333e2434182d9beaa7e29962f9375
Moe Jangda <m.jangda88@gmail.com>
2018-02-23 04:21:56 -0600

    fix basic example

--- examples/javascript/node/basic.js
+++ examples/javascript/node/basic.js
@@ -1,21 +1,27 @@
-var Dropbox = require('../../../lib/dropbox').Dropbox;
-var prompt = require('prompt');
+const prompt = require('prompt');
+
+// This dependency exposes the `fetch` function globally which dropbox-sdk-js depends on.
+// Make sure to require it before requiring the dropbox lib otherwise an error will be
+// thrown immediately.
+require('isomorphic-fetch');
+
+const { Dropbox } = require('dropbox');
 
 prompt.start();
 
 prompt.get({
   properties: {
     accessToken: {
-      description: 'Please enter an API V2 access token'
-    }
-  }
-}, function (error, result) {
-  var dbx = new Dropbox({ accessToken: result.accessToken });
+      description: 'Please enter an API V2 access token',
+    },
+  },
+}, (error, result) => {
+  const dbx = new Dropbox({ accessToken: result.accessToken });
   dbx.filesListFolder({ path: '/Screenshots' })
-    .then(function (response) {
+    .then((response) => {
       console.log(response);
     })
-    .catch(function (err) {
+    .catch((err) => {
       console.log(err);
     });
 });
