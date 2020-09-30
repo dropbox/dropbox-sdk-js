15e40b5df66c3219587e2cbcf17fbe8a8dd98dd3
Brad Rogers <brad12rogers@gmail.com>
2020-09-30 15:35:26 -0700

    Remove release notes and print formatted notes

--- scripts/release_note_generator.sh
+++ scripts/release_note_generator.sh
@@ -4,8 +4,6 @@ last_version=$(git tag -l | tail -n 2 | head -n 1)
 echo "Getting commit history since $last_version"
 num_commits=$(git rev-list --count $last_version..HEAD)
 echo "Found $num_commits commits since last revision"
-log=$(git log -n $num_commits --pretty="format:* %s %n")
-echo $log
-#linked_log=$(echo "Release Notes: \n\n$log" | sed 's/#\([0-9]\+\)/[#\1](link\/\1)/g')
-#echo $linked_log
-echo "Release notes written to release_notes.txt"
+git_log=$(git log -n $num_commits --pretty="format:* %s %n")
+linked_log=$(echo "Release Notes: \n\n$git_log" | sed -e 's/#\([0-9]*\)/[#\1](github.com\/dropbox\/dropbox-sdk-js\/pulls\/\1)/g')
+echo "\n\n$linked_log"
