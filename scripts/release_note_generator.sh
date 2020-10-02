8ca5b6b2d1a93db6363081a19fad72f587005944
Brad Rogers <brad12rogers@gmail.com>
2020-10-02 14:46:58 -0700

    Fix sorting on git tag

--- scripts/release_note_generator.sh
+++ scripts/release_note_generator.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-last_version=$(git tag -l | tail -n 2 | head -n 1)
+last_version=$(git tag --sort v:refname | tail -n 2 | head -n 1)
 echo "Getting commit history since $last_version"
 num_commits=$(git rev-list --count $last_version..HEAD)
 echo "Found $num_commits commits since last revision"
 git_log=$(git log -n $num_commits --pretty="format:* %s %n")
-linked_log=$(echo "Release Notes: \n\n$git_log" | sed -e 's/#\([0-9]*\)/[#\1](github.com\/dropbox\/dropbox-sdk-js\/pulls\/\1)/g')
+linked_log=$(echo "Release Notes: \n\n$git_log" | sed -e 's/#\([0-9]*\)/[#\1](github.com\/dropbox\/dropbox-sdk-js\/pull\/\1)/g')
 echo "\n\n$linked_log"
