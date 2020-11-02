bda44ec005222b7c9dd9db727d64d65a669d2e80
Brad Rogers <brad12rogers@gmail.com>
2020-11-02 14:19:43 -0500

    Fix linter

--- src/response.js
+++ src/response.js
@@ -19,7 +19,7 @@ function throwIfError(res) {
           response: res,
           status: res.status,
         };
-      }, (error) => {
+      }, () => {
         // eslint-disable-next-line no-throw-literal
         throw {
           error: clone.text(),
@@ -27,41 +27,36 @@ function throwIfError(res) {
           status: res.status,
         };
       });
-  } else {
-    return Promise.resolve();
   }
+  return Promise.resolve();
 }
 
 export function parseResponse(res) {
   const clone = res.clone();
 
-  return throwIfError(res).then(() => {
-    return res.json()
-      .then(
-        (data) => new DropboxResponse(res.status, res.headers, data), 
-        (error) => clone.text()
-      );
-  });
+  return throwIfError(res).then(() => res.json()
+    .then(
+      (data) => new DropboxResponse(res.status, res.headers, data),
+      () => clone.text(),
+    ));
 }
 
 export function parseDownloadResponse(res) {
-  return throwIfError(res).then(() => {
-    return new Promise((resolve) => {
-      if (isWindowOrWorker()) {
-        res.blob().then((data) => resolve(data));
-      } else {
-        res.buffer().then((data) => resolve(data));
-      }
-    })
-  }).then((data) => {
-      let result = JSON.parse(res.headers.get('dropbox-api-result'));
+  return throwIfError(res).then(() => new Promise((resolve) => {
+    if (isWindowOrWorker()) {
+      res.blob().then((data) => resolve(data));
+    } else {
+      res.buffer().then((data) => resolve(data));
+    }
+  })).then((data) => {
+    const result = JSON.parse(res.headers.get('dropbox-api-result'));
 
-      if (isWindowOrWorker()) {
-        result.fileBlob = data;
-      } else {
-        result.fileBinary = data;
-      }
-      
-      return new DropboxResponse(res.status, res.headers, result);
-    });
+    if (isWindowOrWorker()) {
+      result.fileBlob = data;
+    } else {
+      result.fileBinary = data;
+    }
+
+    return new DropboxResponse(res.status, res.headers, result);
+  });
 }
