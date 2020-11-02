28417eb780f0cddd4e985aa6d0357d3df1f7b36a
Brad Rogers <brad12rogers@gmail.com>
2020-11-02 14:15:45 -0500

    Unify errors into one function and handle json parsing errors

--- src/response.js
+++ src/response.js
@@ -8,61 +8,60 @@ export class DropboxResponse {
   }
 }
 
-export function parseResponse(res) {
+function throwIfError(res) {
   const clone = res.clone();
-
   if (!res.ok) {
     return res.json()
-      .then((data, error) => {
-        // eslint-disable-next-line no-throw-literal
-        throw {
-          error: error ? clone.text() : data,
-          response: res,
-          status: res.status,
-        };
-      });
-  }
-
-  return res.json()
-    .catch(() => {
-      clone.text();
-    })
-    .then((data) => new DropboxResponse(res.status, res.headers, data));
-}
-
-export function parseDownloadResponse(res) {
-  return new Promise((resolve) => {
-    if (!res.ok) {
-      res.json()
-        .then((data) => resolve(data));
-    } else if (isWindowOrWorker()) {
-      res.blob()
-        .then((data) => resolve(data));
-    } else {
-      res.buffer()
-        .then((data) => resolve(data));
-    }
-  })
-    .then((data) => {
-      let result;
-
-      if (!res.ok) {
+      .then((data) => {
         // eslint-disable-next-line no-throw-literal
         throw {
           error: data,
           response: res,
           status: res.status,
         };
+      }, (error) => {
+        // eslint-disable-next-line no-throw-literal
+        throw {
+          error: clone.text(),
+          response: res,
+          status: res.status,
+        };
+      });
+  } else {
+    return Promise.resolve();
+  }
+}
+
+export function parseResponse(res) {
+  const clone = res.clone();
+
+  return throwIfError(res).then(() => {
+    return res.json()
+      .then(
+        (data) => new DropboxResponse(res.status, res.headers, data), 
+        (error) => clone.text()
+      );
+  });
+}
+
+export function parseDownloadResponse(res) {
+  return throwIfError(res).then(() => {
+    return new Promise((resolve) => {
+      if (isWindowOrWorker()) {
+        res.blob().then((data) => resolve(data));
       } else {
-        result = JSON.parse(res.headers.get('dropbox-api-result'));
-
-        if (isWindowOrWorker()) {
-          result.fileBlob = data;
-        } else {
-          result.fileBinary = data;
-        }
+        res.buffer().then((data) => resolve(data));
       }
+    })
+  }).then((data) => {
+      let result = JSON.parse(res.headers.get('dropbox-api-result'));
 
+      if (isWindowOrWorker()) {
+        result.fileBlob = data;
+      } else {
+        result.fileBinary = data;
+      }
+      
       return new DropboxResponse(res.status, res.headers, result);
     });
 }
