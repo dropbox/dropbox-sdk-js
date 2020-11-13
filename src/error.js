69076db3d89e5472cde8d105999598257c3efaba
Brad Rogers <brad12rogers@gmail.com>
2020-11-13 16:40:37 -0500

    Fix DropboxResponseError class name in UPGRADING.md and add it's fields

--- src/error.js
+++ src/error.js
@@ -1,5 +1,9 @@
 /**
- * The response class of HTTP errors from API calls using the Dropbox SDK.
+ * @class DropboxResponseError
+ * @classdesc The response class of HTTP errors from API calls using the Dropbox SDK.
+ * @arg {number} status - HTTP Status code of the call
+ * @arg {Object} headers - Headers returned from the call
+ * @arg {Object} error - Serialized Error of the call
  */
 export class DropboxResponseError extends Error {
   constructor(status, headers, error) {
