c3444ce30926e993f4de5bd8ba5be4770e68453c
Brad Rogers <brad12rogers@gmail.com>
2021-05-20 17:33:36 -0400

    Update configuration for custom hosts (#661)
    * Update configuration for custom hosts
    
    * Fix linter
    
    * Update tests
    
    * bump version

--- src/utils.js
+++ src/utils.js
@@ -1,13 +1,31 @@
-import { DEFAULT_API_DOMAIN, DEFAULT_DOMAIN } from './constants';
+import { DEFAULT_API_DOMAIN, DEFAULT_DOMAIN, TEST_DOMAIN_MAPPINGS } from './constants';
 
 function getSafeUnicode(c) {
   const unicode = `000${c.charCodeAt(0).toString(16)}`.slice(-4);
   return `\\u${unicode}`;
 }
 
-export const baseApiUrl = (subdomain, domain = DEFAULT_API_DOMAIN, domainDelimiter = '.') => `https://${subdomain}${domainDelimiter}${domain}/2/`;
-export const OAuth2AuthorizationUrl = (domain = DEFAULT_DOMAIN) => `https://${domain}/oauth2/authorize`;
-export const OAuth2TokenUrl = (domain = DEFAULT_API_DOMAIN, domainDelimiter = '.') => `https://api${domainDelimiter}${domain}/oauth2/token`;
+export const baseApiUrl = (subdomain, domain = DEFAULT_API_DOMAIN, domainDelimiter = '.') => {
+  if (domain !== DEFAULT_API_DOMAIN && TEST_DOMAIN_MAPPINGS[subdomain] !== undefined) {
+    subdomain = TEST_DOMAIN_MAPPINGS[subdomain];
+    domainDelimiter = '-';
+  }
+  return `https://${subdomain}${domainDelimiter}${domain}/2/`;
+};
+export const OAuth2AuthorizationUrl = (domain = DEFAULT_DOMAIN) => {
+  if (domain !== DEFAULT_DOMAIN) {
+    domain = `meta-${domain}`;
+  }
+  return `https://${domain}/oauth2/authorize`;
+};
+export const OAuth2TokenUrl = (domain = DEFAULT_API_DOMAIN, domainDelimiter = '.') => {
+  let subdomain = 'api';
+  if (domain !== DEFAULT_API_DOMAIN) {
+    subdomain = TEST_DOMAIN_MAPPINGS[subdomain];
+    domainDelimiter = '-';
+  }
+  return `https://${subdomain}${domainDelimiter}${domain}/oauth2/token`;
+};
 
 // source https://www.dropboxforum.com/t5/API-support/HTTP-header-quot-Dropbox-API-Arg-quot-could-not-decode-input-as/m-p/173823/highlight/true#M6786
 export function httpHeaderSafeJson(args) {
--- test/unit/utils.js
+++ test/unit/utils.js
@@ -24,46 +24,53 @@ describe('Dropbox utils', () => {
     });
   });
 
-  describe('OAuth2AuthorizationUrl', () => {
-    it('correctly returns the authorization url when not provided an override', () => {
-      const testUrl = OAuth2AuthorizationUrl();
-      chai.assert.equal(testUrl, 'https://dropbox.com/oauth2/authorize');
-    });
+  it('correctly sets base url when provided a normal subdomain and domain', () => {
+    const host = 'api';
+    const domain = 'mydomain.com';
+    const testUrl = baseApiUrl(host, domain);
+    chai.assert.equal(testUrl, 'https://api-mydomain.com/2/');
+  });
+});
 
-    it('correctly returns the authorization url when provided an override', () => {
-      const domain = 'mydomain.com';
-      const testUrl = OAuth2AuthorizationUrl(domain);
-      chai.assert.equal(testUrl, 'https://mydomain.com/oauth2/authorize');
-    });
+describe('OAuth2AuthorizationUrl', () => {
+  it('correctly returns the authorization url when not provided an override', () => {
+    const testUrl = OAuth2AuthorizationUrl();
+    chai.assert.equal(testUrl, 'https://dropbox.com/oauth2/authorize');
   });
 
-  describe('OAuth2TokenUrl', () => {
-    it('correctly returns the authorization url when not provided an override', () => {
-      const testUrl = OAuth2TokenUrl();
-      chai.assert.equal(testUrl, 'https://api.dropboxapi.com/oauth2/token');
-    });
+  it('correctly returns the authorization url when provided an override', () => {
+    const domain = 'mydomain.com';
+    const testUrl = OAuth2AuthorizationUrl(domain);
+    chai.assert.equal(testUrl, 'https://meta-mydomain.com/oauth2/authorize');
+  });
+});
 
-    it('correctly returns the authorization url when provided an override', () => {
-      const domain = 'mydomain.com';
-      const testUrl = OAuth2TokenUrl(domain);
-      chai.assert.equal(testUrl, 'https://api.mydomain.com/oauth2/token');
-    });
+describe('OAuth2TokenUrl', () => {
+  it('correctly returns the authorization url when not provided an override', () => {
+    const testUrl = OAuth2TokenUrl();
+    chai.assert.equal(testUrl, 'https://api.dropboxapi.com/oauth2/token');
   });
 
-  describe('getTokenExpiresAtDate', () => {
-    it('correctly sets when the token will expire', () => {
-      const expirationTime = 600;
-      const currentDate = new Date(Date.now());
-      const expiresDate = getTokenExpiresAtDate(expirationTime);
-      // adding a buffer of 100ms to expiration time
-      chai.assert.isTrue((expiresDate - currentDate) / 1000 <= (expirationTime + 100));
-    });
+  it('correctly returns the authorization url when provided an override', () => {
+    const domain = 'mydomain.com';
+    const testUrl = OAuth2TokenUrl(domain);
+    chai.assert.equal(testUrl, 'https://api-mydomain.com/oauth2/token');
   });
+});
 
-  describe('isWindowOrWorker', () => {
-    it('returns false when not window or of type WorkerGlobalScope', () => {
-      const testEnv = isWindowOrWorker();
-      chai.assert.isFalse(testEnv);
-    });
+describe('getTokenExpiresAtDate', () => {
+  it('correctly sets when the token will expire', () => {
+    const expirationTime = 600;
+    const currentDate = new Date(Date.now());
+    const expiresDate = getTokenExpiresAtDate(expirationTime);
+    // adding a buffer of 100ms to expiration time
+    chai.assert.isTrue((expiresDate - currentDate) / 1000 <= (expirationTime + 100));
+  });
+});
+
+describe('isWindowOrWorker', () => {
+  it('returns false when not window or of type WorkerGlobalScope', () => {
+    const testEnv = isWindowOrWorker();
+    chai.assert.isFalse(testEnv);
   });
 });
