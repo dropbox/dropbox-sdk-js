f05f26c7710473081438ead1f808a7f99f64a4bf
Brad Rogers <brad12rogers@gmail.com>
2021-07-28 13:18:56 -0700

    Export DropboxResponse and DropboxResponseError in JS build (#755)
    * Export DropboxResponse and DropboxResponseError in JS build

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 export { default as Dropbox } from './src/dropbox.js';
 export { default as DropboxAuth } from './src/auth.js';
+export { DropboxResponse } from './src/response.js';
+export { DropboxResponseError } from './src/error.js';
